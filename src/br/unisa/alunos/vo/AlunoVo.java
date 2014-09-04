4f4641d9d45746fc1013c6b67bd638b820ca9203
ZYON RODRIGUES DIAS E SILVA <zyon.silva@buscapecompany.com>
2014-09-04 17:02:09 -0300

    criando vo para aluno

---
+++ src/br/unisa/alunos/vo/AlunoVo.java
@@ -0,0 +1,62 @@
+package br.unisa.alunos.vo;
+
+/**
+ * @author zyon.silva
+ *Class para mapear propriedades de um aluno
+ */
+public class AlunoVo {
+	private String nome;
+	private int idade;
+	private String ra;
+	private String curso;
+	
+	
+	/**
+	 * @return {@link String} nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+	/**
+	 * @param nome
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	/**
+	 * @return {@link Integer} idade do aluno
+	 */
+	public int getIdade() {
+		return idade;
+	}
+	/**
+	 * @param idade
+	 */
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	/**
+	 * @return {@link String} ra do aluno
+	 */
+	public String getRa() {
+		return ra;
+	}
+	/**
+	 * @param ra
+	 */
+	public void setRa(String ra) {
+		this.ra = ra;
+	}
+	/**
+	 * @return {@link String} nome do curso
+	 */
+	public String getCurso() {
+		return curso;
+	}
+	/**
+	 * @param curso
+	 */
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+}
