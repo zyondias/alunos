fd451b68c4d9feb856beb28e62900df4080a1e9b
Zyon Dias <zyon.dias@hotmail.com>
2014-09-06 00:11:28 -0300

    refatorando codigo para deixar impressao de lista de alunos melhor

--- src/br/unisa/alunos/servlets/ListarAlunosServlet.java
+++ src/br/unisa/alunos/servlets/ListarAlunosServlet.java
@@ -2,6 +2,7 @@ package br.unisa.alunos.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -18,17 +19,28 @@ public class ListarAlunosServlet extends HttpServlet{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	private PrintWriter out;
 
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
-		PrintWriter out = response.getWriter();
-		out.print("<html><head></head><body><h3>Lista de alunos</h3>");
-		for(AlunoVo aluno : AlunoRepository.buscaAlunos()){
-			out.print(aluno+"<br />");
-		}
+		out = response.getWriter();
+		out.print("<html><head><title>Lista Alunos</title></head><body><h3>Lista de alunos</h3>");
+		imprimirAlunosEmLista(AlunoRepository.buscaAlunos());
 		out.print("<br /><a href=cadastro-aluno.html>Cadastra Aluno</a></body></html>");
+	}
+	
+	private void imprimirAlunosEmLista(List<AlunoVo> alunos){
+		out.print("<table><tr><th>Nome</th><th>Idade</th><th>Ra</th><th>Curso</th></tr>");
+		for(AlunoVo aluno : alunos){
+			out.print("<tr><td>"+aluno.getNome()+"</td>");
+			out.print("<td>"+aluno.getIdade()+"</td>");
+			out.print("<td>"+aluno.getRa()+"</td>");
+			out.print("<td>"+aluno.getCurso()+"</td></tr>");
+		}
+		out.print("</table>");
 	}
 
 }
